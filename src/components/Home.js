7e28c623ed7e0eff57494b211de6b6f9ae84805b
John-JacobP <John.JacobP@ust.com>
2024-01-01 13:16:46 +0530

    Format Changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,33 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
-
 import "./Home.css"
 import GridLayout from 'react-grid-layout';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
-import { AzureWidgets } from "./AzureWidgets";
-
+import {AzureWidgets} from "./AzureWidgets";
 
 const Header = styled.h3`
-    color: white;
-    padding: 1em;
+  color: white;
+  padding: 1em;
 `;
 
 const Widget = styled.div`
-    border: 1px solid black;
-    background: ivory;
+  border: 1px solid black;
+  background: ivory;
 `;
-
-
 
 const Home = () => {
 
-
- const [layoutConfig, updateLayoutConfig] = useState([
+    const [layoutConfig, updateLayoutConfig] = useState([
         {i: 'item1', x: 0, y: 0, w: 2, h: 3},
         {i: 'item2', x: 2, y: 0, w: 4, h: 3},
         {i: 'item3', x: 6, y: 0, w: 2, h: 3}
     ]);
 
- const handleDrop = (layout, layoutItem, _event) => {
-    const newLayoutItem = {...layoutItem, i: "VM"}
-    updateLayoutConfig([...layoutConfig, newLayoutItem]);
-}  
+    const handleDrop = (layout, layoutItem, _event) => {
+        const newLayoutItem = {...layoutItem, i: "VM"}
+        updateLayoutConfig([...layoutConfig, newLayoutItem]);
+    }
     const GridLayoutProps = {
         className: "my-grid",
         cols: 12,
@@ -48,11 +43,11 @@ const Home = () => {
             </div>
             <div className="sidebarandbody">
                 <div className="sidebar">
-                    <AzureWidgets />
+                    <AzureWidgets/>
                 </div>
                 <div className="body">
                     <div className="insideBody">
-                        <GridLayout 
+                        <GridLayout
                             {...GridLayoutProps}
                             isDroppable={true}
                             onDrop={handleDrop}
