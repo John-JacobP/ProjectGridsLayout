0f1be4770dd0002a0afed9c22e24d65260c98f08
shn-ust <142196840+shn-ust@users.noreply.github.com>
2024-01-01 07:37:47 +0000

    Azure widgets.

---
+++ src/components/AzureWidgets.jsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+
+const DroppableWidget = styled.div`
+    border: 1px solid black;
+    border-radius: 5px;
+    background-color: white;
+    width: 30px;
+    height: 30px
+`;
+
+
+
+export const AzureWidgets = () => {
+    return (
+        <>
+            Azure Widgets
+
+            <DroppableWidget
+                draggable={true}
+                unselectable="on"
+                // this is a hack for firefox
+                // Firefox requires some kind of initialization
+                // which we can do by adding this attribute
+                // @see https://bugzilla.mozilla.org/show_bug.cgi?id=568313
+                onDragStart={e => e.dataTransfer.setData("text/plain", "")}
+                key="VM"
+            >
+                VM
+            </DroppableWidget>
+        </>
+    );
+}
