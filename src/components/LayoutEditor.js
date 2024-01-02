9f77f29a6e378043edf822c099fe4b3ea8d31a9e
John-JacobP <John.JacobP@ust.com>
2024-01-02 14:27:30 +0530

    LayoutEditor conflict resolved

--- src/components/LayoutEditor.js
+++ src/components/LayoutEditor.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import GridLayout from "react-grid-layout";
-import { AzureWidgets } from "./AzureWidgets";
-
 import "react-grid-layout/css/styles.css";
 import "react-resizable/css/styles.css";
+import { AzureWidgets } from "./AzureWidgets";
 import "./LayoutEditor.css";
 
 const Header = styled.h3`
@@ -17,52 +16,65 @@ const Widget = styled.div`
   background: ivory;
 `;
 
-export default function LayoutEditor() {
+const LayoutEditor = () => {
   const [layoutConfig, updateLayoutConfig] = useState([
     { i: "Test", x: 0, y: 1, h: 1, w: 1 },
   ]);
+
+  const [counter, setCounter] = useState(0);
 
   const [_droppingItem, updateDroppingItem] = useState("");
 
   const handleDrop = (layout, layoutItem, _event) => {
     // Function to add the new widget to the layout
+    const newLayoutItem = {
+      i: `${_droppingItem}-${counter}`,
+      x: 0, // Set the desired x coordinate
+      y: 0, // Set the desired y coordinate
+      w: 1, // Adjust the width based on your design
+      h: 1,
+    };
 
-    const newLayoutItem = { ...layoutItem };
     updateLayoutConfig([...layoutConfig, newLayoutItem]);
+    setCounter((prevCounter) => prevCounter + 1);
   };
 
   const GridLayoutProps = {
-    className: "my-grid",
+    className: "Grid",
     cols: 12,
     rowHeight: 150,
     width: 1100,
-    // Layout: { ...layoutConfig },
   };
 
   return (
-    <div className="FullPage">
-      <div className="TopNav">
-        <Header>Layout Editor</Header>
-      </div>
-      <div className="sidebarandbody">
-        <div className="sidebar">
-          <AzureWidgets updateStateCallback={updateDroppingItem} />
+      <div className="FullPage">
+        <div className="TopNav">
+          <Header>Layout Editor</Header>
         </div>
-        <div className="body">
-          <div className="insideBody">
-            <GridLayout
-              {...GridLayoutProps}
-              isDroppable={true}
-              onDrop={handleDrop}
-              droppingItem={{ i: _droppingItem, w: 1, h: 1 }}
+        <div className="sidebarandbody">
+          <div className="sidebar">
+            <AzureWidgets updateStateCallback={updateDroppingItem} />
+          </div>
+          <div className="body">
+            <div
+                className="insideBody"
+                onDrop={(event) => {
+                  handleDrop(null, event);
+                }}
+                onDragOver={(event) => event.preventDefault()}
             >
-              {layoutConfig.map((layout) => (
-                <Widget key={layout.i}>{layout.i}</Widget>
-              ))}
-            </GridLayout>
+              <GridLayout {...GridLayoutProps} isDroppable={true}>
+                {layoutConfig.map((layout) => (
+                    <Widget key={layout.i} data-grid={layout}>
+                      {layout.i}
+                    </Widget>
+                ))}
+              </GridLayout>
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
-}
+};
+
+export default LayoutEditor;
