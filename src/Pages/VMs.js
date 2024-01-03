4f1284d44f17553bd1e9247df808ce7984ea6817
John-JacobP <John.JacobP@ust.com>
2024-01-03 18:18:25 +0530

    VM Page initial Commit

---
+++ src/Pages/VMs.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./VMs.css";
+
+const VMs = () => {
+    return(
+      <div className="FullPage">
+          <div className="NavBar">
+              <p>Nav</p>
+          </div>
+          <div className="SideBar">
+              <p>Sidebar</p>
+          </div>
+          <div className="body">
+              <p>BODY</p>
+          </div>
+      </div>
+    );
+};
+
+export default VMs;
