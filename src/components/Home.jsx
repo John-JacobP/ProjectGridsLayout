26d48c400ff1cb34ece04e90d01cd200d8cd5c30
John-JacobP <John.JacobP@ust.com>
2024-01-04 14:55:29 +0530

    added nav and sidebar for home and css changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import ReactGridLayout from "react-grid-layout";
 import styled from "styled-components";
 import "./Home.css";
@@ -24,32 +25,44 @@ export default function Home() {
 
     return (
         <>
-            {/* Conditional rendering based on whether there's a layout or not */}
-            {layout.length > 0 ? (
-                <div className="HomePage">
-                    <ReactGridLayout
-                        isDraggable={false}
-                        isDroppable={false}
-                        isResizable={false}
-                        layout={layout}
-                        cols={12}
-                        width={1300}
-                        rowHeight={100}
-                        compactType={null}
-                        className="GridLayoutHome"
-                    >
-                        {/* Render widgets based on the current layout configuration */}
-                        {layout.map((widget) => (
-                            <Widget className="widgets" key={widget.i}>
-                                {widget.i}
-                            </Widget>
-                        ))}
-                    </ReactGridLayout>
+            <div className="FullPage">
+                <div className="NavBar">Nav</div>
+                <div className="SideBar">Side</div>
+                <div className="body">
+                    <div className="savebutton">
+                        {/* Use Link to navigate to the LayoutEditor page and pass the layout as state */}
+                        <Link to="/editor">
+                            <button style={{ cursor: "pointer" }}>Edit Layout</button>
+                        </Link>
+                    </div>
+                    <div className="insideBody">
+                        {/* Conditional rendering based on whether there's a layout or not */}
+                        {layout.length > 0 ? (
+                            <ReactGridLayout
+                                isDraggable={false}
+                                isDroppable={false}
+                                isResizable={false}
+                                layout={layout}
+                                cols={12}
+                                width={1300}
+                                rowHeight={100}
+                                compactType={null}
+                                className="GridLayoutHome"
+                            >
+                                {/* Render widgets based on the current layout configuration */}
+                                {layout.map((widget) => (
+                                    <Widget className="widgets" key={widget.i}>
+                                        {widget.i}
+                                    </Widget>
+                                ))}
+                            </ReactGridLayout>
+                        ) : (
+                            // Message for when there's no layout configured
+                            <div>Please create a layout.</div>
+                        )}
+                    </div>
                 </div>
-            ) : (
-                // Message for when there's no layout configured
-                <div>Please create a layout.</div>
-            )}
+            </div>
         </>
     );
 }
