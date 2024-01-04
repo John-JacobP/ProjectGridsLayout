2aa321e485f17800d01e99fcf885ecdbc4e9a6a3
John-JacobP <John.JacobP@ust.com>
2024-01-04 18:19:19 +0530

    Route added for VM Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import "./App.css";
+import VMs from "./Pages/VMs";
 
 import Dashboard from "./components/Dashboard";
 import LayoutEditor from "./components/LayoutEditor";
 import Home from "./components/Home";
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/editor" element={<LayoutEditor />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/dashboard" element={<Dashboard/>}/>
+                    <Route path="/editor" element={<LayoutEditor/>}/>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/VMs" element={<VMs/>}/>
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
