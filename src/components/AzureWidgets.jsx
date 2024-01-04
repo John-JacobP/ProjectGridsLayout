cb357ff5d8ac5183e1d8c58b507c975c4e595870
shn <142196840+shn-ust@users.noreply.github.com>
2024-01-04 09:17:37 +0000

    WidgetBuilder update.

--- src/components/AzureWidgets.jsx
+++ src/components/AzureWidgets.jsx
@@ -10,29 +10,36 @@ const DraggableWidget = styled.div`
   margin: 2px 2px 5px 2px;
 `;
 
-const widgetList = ["VM", "SQL", "Resource Group", "Users", "Load Balancers"];
+// const widgetList = ["VM", "SQL", "Resource Group", "Users", "Load Balancers"];
+export const widgetList = [
+  { id: "azure-vm", name: "Virtual Machine", provider: "azure" },
+  { id: "azure-sql", name: "SQL", provider: "azure" },
+  { id: "azure-rg", name: "Resource Group", provider: "azure" },
+  { id: "azure-users", name: "Users", provider: "azure" },
+  { id: "azure-load_balancer", name: "Load Balancer", provider: "azure" },
+];
 
 const handleDrag = (widget, updateStateCallback) => {
-    // updates `_droppingItem` in Home.js
+  // updates `_droppingItem` in Home.js
 
-    updateStateCallback(widget);
+  updateStateCallback(widget);
 };
 
-export const AzureWidgets = ({updateStateCallback}) => {
-    return (
-        <>
-            <h4>Azure Widgets</h4>
-            <div className="widgets">
-                {widgetList.map((widget, index) => (
-                    <DraggableWidget
-                        key={index}
-                        draggable={true}
-                        onDrag={() => handleDrag(widget, updateStateCallback)}
-                    >
-                        {widget}
-                    </DraggableWidget>
-                ))}
-            </div>
-        </>
-    );
+export const AzureWidgets = ({ updateStateCallback }) => {
+  return (
+    <>
+      <h4>Azure Widgets</h4>
+      <div className="widgets">
+        {widgetList.map((widget, index) => (
+          <DraggableWidget
+            key={index}
+            draggable={true}
+            onDrag={() => handleDrag(widget.id, updateStateCallback)}
+          >
+            {widget.name}
+          </DraggableWidget>
+        ))}
+      </div>
+    </>
+  );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import ReactGridLayout from "react-grid-layout";
 import styled from "styled-components";
 import "./Home.css";
+import { WidgetBuilder } from "../WidgetBuilder";
 
 // Styled component for widget styling
 const Widget = styled.div`
@@ -11,45 +12,50 @@ const Widget = styled.div`
 
 // Home component for rendering the layout
 export default function Home() {
-    // State for managing the layout configuration
-    const [layout, setLayout] = useState([]);
+  // State for managing the layout configuration
+  const [layout, setLayout] = useState([]);
 
-    // hook to load the layout from local storage on component mount
-    useEffect(() => {
-        const storedLayout = localStorage.getItem("layout");
-        if (storedLayout) {
-            setLayout(JSON.parse(storedLayout));
-        }
-    }, []);
+  // hook to load the layout from local storage on component mount
+  useEffect(() => {
+    const storedLayout = localStorage.getItem("layout");
+    if (storedLayout) {
+      setLayout(JSON.parse(storedLayout));
+    }
+  }, []);
 
-    return (
-        <>
-            {/* Conditional rendering based on whether there's a layout or not */}
-            {layout.length > 0 ? (
-                <div className="HomePage">
-                    <ReactGridLayout
-                        isDraggable={false}
-                        isDroppable={false}
-                        isResizable={false}
-                        layout={layout}
-                        cols={12}
-                        width={1300}
-                        rowHeight={100}
-                        compactType={null}
-                        className="GridLayoutHome"
-                    >
-                        {/* Render widgets based on the current layout configuration */}
-                        {layout.map((widget) => (
-                            <Widget className="widgets" key={widget.i}>
-                                {widget.i}
-                            </Widget>
-                        ))}
-                    </ReactGridLayout>
-                </div>
-            ) : (
-                // Message for when there's no layout configured
-                <div>Please create a layout.</div>
-            )}
-        </>
-    );
+  return (
+    <>
+      {/* Conditional rendering based on whether there's a layout or not */}
+      {layout.length > 0 ? (
+        <div className="HomePage">
+          <ReactGridLayout
+            isDraggable={false}
+            isDroppable={false}
+            isResizable={false}
+            layout={layout}
+            cols={12}
+            width={1300}
+            rowHeight={100}
+            compactType={null}
+            className="GridLayoutHome"
+          >
+            {/* Render widgets based on the current layout configuration */}
+            {layout.map((widget) => (
+              <Widget className="widgets" key={widget.i}>
+                {widget.i}
+              </Widget>
+            ))}
+            {/* {
+              layout.map((widget, index) => (
+                <WidgetBuilder key={index} widget={widget}/>
+              ))
+            } */}
+          </ReactGridLayout>
+        </div>
+      ) : (
+        // Message for when there's no layout configured
+        <div>Please create a layout.</div>
+      )}
+    </>
+  );
 }
